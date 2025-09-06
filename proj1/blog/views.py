d76296299690684bc76eee450e83567aa886078e
Pranav Satish <pranavs13@gmail.com>
2025-09-06 23:54:11 +0530

    Added styling and posts

--- proj1/blog/views.py
+++ proj1/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "Pranav S",
+        "title" : "Blog Post 1",
+        "date_posted": "September 6 2025",
+        "content": "Content 1",
+    },
+    {
+        "author": "John Doe",
+        "title" : "Blog Post 2",
+        "date_posted": "September 5 2025",
+        "content": "Content 2",
+    },
+]
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        "posts": posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {"title": "About"})
