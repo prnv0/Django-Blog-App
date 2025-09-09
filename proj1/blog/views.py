b95380b85fb5353fe22c02e7b11a7d05d50023c2
Pranav Satish <pranavs13@gmail.com>
2025-09-09 22:10:12 +0530

    Added post model

--- proj1/blog/views.py
+++ proj1/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        "author": "Pranav S",
-        "title" : "Blog Post 1",
-        "date_posted": "September 6 2025",
-        "content": "Content 1",
-    },
-    {
-        "author": "John Doe",
-        "title" : "Blog Post 2",
-        "date_posted": "September 5 2025",
-        "content": "Content 2",
-    },
-]
 # Create your views here.
 def home(request):
     context = {
-        "posts": posts,
+        "posts": Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
